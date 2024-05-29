49e021392b65ce404b6e6ff76437f257baa924fb
iris <laanearu.iris@gmail.com>
2024-05-29 09:16:56 +0000

    only ROS package

--- RT2_Assignment_2/build/actionlib_tutorials/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/root/RT1_Assignment_2/devel/include".split(';') if "/root/RT1_Assignment_2/devel/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "actionlib_tutorials"
-PROJECT_SPACE_DIR = "/root/RT1_Assignment_2/devel"
-PROJECT_VERSION = "0.0.0"
--- RT2_Assignment_2/build/second_assignment/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/RT1_Assignment_2/src/second_assignment
-# Build directory: /root/RT1_Assignment_2/build/second_assignment
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
